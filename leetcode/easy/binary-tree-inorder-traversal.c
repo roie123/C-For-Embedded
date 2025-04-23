61bf5e4a0187c4b03420d0eadde21a4652002487
roie123 <roieivri123@gmail.com>
2025-04-23 14:15:11 +0300

    solution for  Binary Tree Inorder Traversal recursive solution, beats 100% Question : https://leetcode.com/problems/binary-tree-inorder-traversal/description/
    my submission :
    https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/1615586953/

--- leetcode/easy/binary-tree-inorder-traversal.c
+++ leetcode/easy/binary-tree-inorder-traversal.c
@@ -1,7 +1,6 @@
-
-
-
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct TreeNode {
     int val;
@@ -9,18 +8,59 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-struct stack {
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
 
-};
+
+
+void inorder(struct TreeNode *root, int *arr, int *index) {
+    if (root == NULL) return;
+    inorder(root->left,arr,index);
+    arr[(*index)++] = root->val;
+    inorder(root->right,arr,index);
+}
 
 int *inorderTraversal(struct TreeNode *root, int *returnSize) {
-int arr[*returnSize]={NULL};
-
-
-
-
-
+    int *result = calloc(100 , sizeof(int));
+    int index = 0;
+    inorder(root, result,&index);
+    *returnSize = index;
+    return result;
 }
 
 int main() {
+    struct TreeNode* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->right = newNode(8);
+
+    root->left->right->left = newNode(6);
+    root->left->right->right = newNode(7);
+    root->right->right->left = newNode(9);
+
+
+
+    // Run traversal
+    int returnSize;
+    int* output = inorderTraversal(root, &returnSize);
+
+    // Print result
+    printf("Inorder Traversal: ");
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", output[i]);
+    }
+    printf("\n");
+
+    // Free memory (basic cleanup)
+    free(output);
+    // (free tree no
+
 }
