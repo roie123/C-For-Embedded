9f2d9610ee16e8a7474d3cc97dd392090f5b1dee
roie123 <roieivri123@gmail.com>
2025-04-29 13:14:39 +0300

    my solution for remove element
    my submission
    https://leetcode.com/problems/remove-element/submissions/1620984071/

--- leetcode/easy/remove_element.c
+++ leetcode/easy/remove_element.c
@@ -1,54 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int removeElement(int *nums, int numsSize, int val);
-
-int main(void) {
-    int arr[8]
-            = {
-                4, 3, 1, 3, 1, 2, 3, 7
-            };
-    for (int i = 0; i < 8; ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf(" the return %d\n", removeElement(arr, 8, 3));
-    for (int i = 0; i < 8; ++i) {
-        printf("%d", arr[i]);
-    }
-    return 0;
+// Comparison function for qsort
+int cmp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
 
-int removeElement(int *nums, int numsSize, int val) {
-    int p = 0;
-    int q = (numsSize - 1);
-    int sum = 0;
+// Assume this is implemented elsewhere
+int removeElement(int *nums, int numsSize, int val);
 
-    while (p != q || p < q) {
-        printf("\n P:[%d]%d  Q:(%d)%d \n",p,nums[p],q,nums[q]);
-        if (nums[p] == val) {
-            if (nums[q]==val) {
-                q--;
-                continue;
-            }else {
-                nums[p]=nums[q];
-                nums[q]=val;
-                p++;
-                q--;
-                continue;
-            }
+int main() {
+    int nums[] = {3, 2, 2, 3};
+    int val = 3;
+    int expectedNums[] = {2, 2};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int expectedLength = sizeof(expectedNums) / sizeof(expectedNums[0]);
 
+    int k = removeElement(nums, numsSize, val);
 
-        }else {
-            if (nums[q]==val) {
-                q--;
-                p++;
-                continue;
-            }else {
-                p++;
-                q--;
+    assert(k == expectedLength);
 
-            }
-        }
+    // Sort the first k elements of nums
+    qsort(nums, k, sizeof(int), cmp);
+
+    for (int i = 0; i < expectedLength; i++) {
+        assert(nums[i] == expectedNums[i]);
     }
 
-    return sum;
+    printf("Test passed!\n");
+    return 0;
+}
+int removeElement(int* nums, int numsSize, int val) {
+    int i = 0, j = 0;
+    int sum=0;
+    while (i < numsSize) {
+        if (nums[i] == val) { //nums[i] is value
+            if (i==numsSize-1) { //if we are at the last index
+                break;
+            }else {
+                j=i+1;
+                while (j<numsSize) {
+                    if (nums[j]!=val) {//nums[j] is not value
+                        nums[i]=nums[j];
+                        nums[j]=val;
+                        i++;
+                        continue;
+                    }else {
+                        j++;
+                    }
+                }
+                i++;
+            }
+        }else {// nums[i] is not value
+            i++;
+
+        }
+    }
+i=0;
+while(i<numsSize) {
+    if (nums[i]!=val) {
+        sum++;
+        i++;
+    }else {
+        i++;
+    }
+}
+return sum;
+
 }
